237d8a4251124b76ba1fdb9e84e8a72ce31acda2
nandhananaren <nandhananaren@gmail.com>
2026-03-14 01:30:41 +0530

    Initial commit

---
+++ src/app/components/HabitItem.jsx
@@ -0,0 +1,36 @@
+export default function HabitItem({
+  name,
+  description,
+  streak,
+  longestStreak,
+  onComplete
+}) {
+  return (
+    <div className="border rounded-lg p-4 shadow mb-4">
+
+      <h2 className="text-lg font-semibold">
+        {name}
+      </h2>
+
+      <p className="text-sm text-gray-600">
+        {description}
+      </p>
+
+     <p className="text-sm text-gray-600">
+    Total Completions: {streak}
+    </p>
+
+      <p className="text-sm text-gray-600">
+        Longest Streak: {longestStreak}
+      </p>
+
+    <button
+    onClick={onComplete}
+    className="mt-3 px-2 py-1 border border-black text-sm"
+  >
+  Mark Complete
+</button>
+
+    </div>
+  );
+}
